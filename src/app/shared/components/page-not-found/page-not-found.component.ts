c90d7f91fe4434fcdea9bce03a8df57ac496a446
saeedshojaei <aliakbaresmaeili98@gmail.com>
2023-01-14 19:31:52 +0330

    handle cancel item and upload item

--- src/app/shared/components/page-not-found/page-not-found.component.ts
+++ src/app/shared/components/page-not-found/page-not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,8 +6,11 @@ import { Router } from '@angular/router';
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit {
   constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
   routerComment() {
     this.router.navigateByUrl('/log-in');
   }
