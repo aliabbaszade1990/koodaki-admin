131c5ec303f615157411fe6d794766ec2a4f5870
Ali Hasani <alihasani.get@gmail.com>
2023-05-29 18:21:55 +0330

    Show error on login

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/modules/core/services/auth.service';
+import { NotificationService } from 'src/app/shared/services/notification.service';
 import { LoginResultDTO } from '../dtos/auth.dto';
 
 @Component({
@@ -14,7 +15,8 @@ export class LoginComponent implements OnInit {
   constructor(
     private router: Router,
     private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private notification: NotificationService
   ) {}
 
   ngOnInit() {
@@ -27,11 +29,16 @@ export class LoginComponent implements OnInit {
   inProgress = false;
   login(): void {
     this.inProgress = true;
-    this.authService.login(this.form.value).then((result: LoginResultDTO) => {
-      if (result) {
-        this.router.navigate(['customer/list']);
-      }
-    });
+    this.authService
+      .login(this.form.value)
+      .then((result: LoginResultDTO) => {
+        if (result) {
+          this.router.navigate(['customer/list']);
+        }
+      })
+      .catch((error) => {
+        this.notification.error(error.message);
+      });
   }
 
   get f() {
