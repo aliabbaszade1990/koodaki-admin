3cc9d2776ff6db45fa1204c6180068d01dedcf44
Ali Hasani <alihasani.get@gmail.com>
2023-06-07 20:27:01 +0330

    No Data Found

--- src/app/modules/project/image-list/image-list.module.ts
+++ src/app/modules/project/image-list/image-list.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { NoDataFoundModule } from 'src/app/shared/modules/no-data-found/shared-ui-no-data-found.module';
 import { ImageListComponent } from './image-list/image-list.component';
 
 @NgModule({
   declarations: [ImageListComponent],
-  imports: [CommonModule, MatIconModule, NgOptimizedImage],
+  imports: [CommonModule, MatIconModule, NgOptimizedImage, NoDataFoundModule],
   exports: [ImageListComponent],
 })
 export class ImageListModule {}
--- src/app/shared/modules/no-data-found/no-data-found.component.ts
+++ src/app/shared/modules/no-data-found/no-data-found.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'koodaki-no-data-found',
   templateUrl: './no-data-found.component.html',
   styleUrls: ['./no-data-found.component.scss'],
 })
-export class NoDataFoundComponent {}
+export class NoDataFoundComponent {
+  @Input() title: string = '';
+  @Input() icon: string = '';
+}
